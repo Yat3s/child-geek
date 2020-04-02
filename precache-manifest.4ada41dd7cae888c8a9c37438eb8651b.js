2a6babdaa06525f49801fe7f2ce6d7e1c46f7a36
Chris Ye <zhiye@microsoft.com>
2020-04-02 15:24:21 +0800

    Init

---
+++ precache-manifest.4ada41dd7cae888c8a9c37438eb8651b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ab1c6baf2955abed7fc99c10709c90",
+    "url": "./index.html"
+  },
+  {
+    "revision": "021305303152b46ad8d0",
+    "url": "./static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "f3b79abb1afc0dd58caf",
+    "url": "./static/css/main.edc3d7ce.chunk.css"
+  },
+  {
+    "revision": "021305303152b46ad8d0",
+    "url": "./static/js/2.a4f36887.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.a4f36887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3b79abb1afc0dd58caf",
+    "url": "./static/js/main.60b329b6.chunk.js"
+  },
+  {
+    "revision": "e3c90d7bc7ce23936cf2",
+    "url": "./static/js/runtime-main.52badf97.js"
+  },
+  {
+    "revision": "dbe42a12289bde1df6c6c09e49209ae7",
+    "url": "./static/media/Skia.dbe42a12.ttf"
+  },
+  {
+    "revision": "259edafce5925f0a78e72ce86c4d1247",
+    "url": "./static/media/res1.259edafc.png"
+  },
+  {
+    "revision": "d494b6a647f5b1c67ddd53fe55136d89",
+    "url": "./static/media/res2.d494b6a6.png"
+  },
+  {
+    "revision": "fb291d211c955e69febfb4b50331a68f",
+    "url": "./static/media/res3.fb291d21.png"
+  },
+  {
+    "revision": "c6000861c6f93880a086ef4e0ea17058",
+    "url": "./static/media/res4.c6000861.png"
+  },
+  {
+    "revision": "922cc1c1652f83a70ea5c63e214e4ccf",
+    "url": "./static/media/res5.922cc1c1.png"
+  },
+  {
+    "revision": "4f9a57f33e53ebf9dd43df9f53b4e688",
+    "url": "./static/media/vaccine.4f9a57f3.png"
+  }
+]);
